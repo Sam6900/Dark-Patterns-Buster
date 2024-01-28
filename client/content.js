0a12356b80ad47aaa7a65be149261de6a189e4f2
Sahil Sharma <vlsharma713@gmail.com>
2024-01-29 03:41:56 +0530

    Add CORS to api

--- client/content.js
+++ client/content.js
@@ -1,44 +1,40 @@
+const endpoint = "http:/127.0.0.1:5000/";
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("Inside Message", message.message);
     if (message.message === "restart") {
 		console.log("restart message received");
-		scrapeText();
+		getDarkPatternNodes();
     } else if (message.message === "check") {
         sendResponse({message: "present"});
     } 
 })
 
 
-function scrapeText() {
-	const treeWalker = document.createTreeWalker(
-		document.body,
-		NodeFilter.SHOW_TEXT,
-	);
-	
-	const filteredNodes = [];
-	const filteredTexts = [];
-
-	while (treeWalker.nextNode()) {
-		const textNode = treeWalker.currentNode;
-		const parentNode = textNode.parentNode;
-
-		if (
-			parentNode.tagName &&
-			ignoredElements.includes(parentNode.tagName.toLowerCase())
-		) {
-			continue;
+function getDarkPatternNodes() {
+	const {filteredNodes, filteredTexts} = scrapeText();
+	const darkPatternNodes = [];
+	fetch (endpoint, {
+		method: "POST",
+		body: JSON.stringify({tokens: filteredTexts}),
+		headers: {"Content-Type": "application/json"}
+	}).then(response => response.json())
+	.then(data => {
+		console.log(data);
+		for (let i = 0; i < data.length; i++) {
+			if (data[i].label === 'darkpattern' && data[i].score > 0.70) {
+				darkPatternNodes.push(filteredNodes[i]);
+			}
 		}
-
-		const trimmedText = textNode.nodeValue.trim();
-		if (trimmedText.split(" ").length > 2) {
-			filteredNodes.push(textNode);
-			filteredTexts.push(trimmedText);
-		}
-	}
-	console.log(filteredTexts);
+		highlightNodes(darkPatternNodes);
+	}).catch(err => {
+		console.log("Got an error", err);
+	});
 }
 
 
-
-
-
+function highlightNodes(nodes) {
+	for (const node of nodes) {
+		console.log(node.parentNode);
+		node.parentNode.style.backgroundColor = "red";
+	}
+}
--- client/utils.js
+++ client/utils.js
@@ -1,1 +1,34 @@
-const ignoredElements = ["script", "style", "noscript", "br", "hr", "code", "nav", "span"];
+const ignoredElements = ["script", "style", "noscript", "br", "hr", "code", "nav"];
+
+
+function scrapeText() {
+	const treeWalker = document.createTreeWalker(
+		document.body,
+		NodeFilter.SHOW_TEXT,
+	);
+	
+	const filteredNodes = [];
+	const filteredTexts = [];
+
+	while (treeWalker.nextNode()) {
+		const textNode = treeWalker.currentNode;
+		const parentNode = textNode.parentNode;
+
+		if (
+			parentNode.tagName &&
+			ignoredElements.includes(parentNode.tagName.toLowerCase())
+		) {
+			continue;
+		}
+
+		const trimmedText = textNode.nodeValue.trim();
+		if (trimmedText.split(" ").length > 3) {
+			filteredNodes.push(textNode);
+			filteredTexts.push(trimmedText);
+		}
+	}
+    console.log(filteredTexts);
+	return {filteredNodes, filteredTexts};
+}
+
+
